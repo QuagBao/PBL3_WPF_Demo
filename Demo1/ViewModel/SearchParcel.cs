de6049c66c9bd989e46e0a09a2b7088da443c9a5
HoangLeAnhTuan <hoangleanhtuannk@gmail.com>
2023-05-08 17:35:17 +0700

    aaa

---
+++ Demo1/ViewModel/SearchParcel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Demo1.ViewModel
+{
+    public class SearchParcel : BaseViewModel
+    {
+        private string searchText;
+        private ObservableCollection<string> searchResults;
+
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                if (searchText != value)
+                {
+                    searchText = value;
+                    OnPropertyChanged(nameof(SearchText));
+                }
+            }
+        }
+
+        public ObservableCollection<string> SearchResults
+        {
+            get { return searchResults; }
+            set
+            {
+                if (searchResults != value)
+                {
+                    searchResults = value;
+                    OnPropertyChanged(nameof(SearchResults));
+                }
+            }
+        }
+
+        public ICommand SearchCommand { get; set; }
+
+        public SearchParcel()
+        {
+            SearchCommand = new RelayCommand<object>((p) =>
+            {
+                return !string.IsNullOrEmpty(SearchText);
+            }, (p) =>
+            {
+                Search(SearchText);
+            });
+        }
+
+        void Search(string value)
+        {
+            /*if (value == "123")
+            {
+                SearchResults = new ObservableCollection<string>
+                {
+                    "Kết quả 1",
+                    "Kết quả 2",
+                    "Kết quả 3"
+                };
+            }
+            else
+            {
+                MessageBox.Show("Hông có kết quả bé ơi");
+            }*/
+            using (var context = new Model.PBL3_demoEntities())
+            {
+                var customerIDCheck = context.Customers.Where(x => x.customerID == value);
+                if (customerIDCheck.Any())
+                {
+                    SearchResults = new ObservableCollection<string>
+                    {
+                        customerIDCheck.ToString()
+                    };
+                }
+                else
+                {
+                    MessageBox.Show("Không có đơn hàng này trong hệ thống");
+                }
+            }
+        }
+    }
+}
